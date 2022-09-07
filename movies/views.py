3ea8cdf116c73501f7b6eed15020c2578f543c93
Ruslan <ruslan_ru911@mail.ru>
2022-09-07 15:53:06 +0300

    07.09.2022

--- movies/views.py
+++ movies/views.py
@@ -1,36 +1,64 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.urls import reverse_lazy
 from .models import *
 from .forms import *
+from django.views.generic import ListView, DetailView, CreateView
+from .utils import *
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-def index(request):
-    posts = Movies.objects.all()
+class MoviesHome(DataMixin, ListView):
+    model = Movies
+    template_name = 'movies/index.html'
+    context_object_name = 'posts'
 
-    context = {
-        'posts': posts,
-        'title': 'Главная страница',
-        'cat_selected': 0,
-    }
-    return render(request, 'movies/index.html', context=context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Главная страница')
+        return dict(list(context.items()) + list(c_def.items()))
 
+    def get_queryset(self):
+        return Movies.objects.filter(is_published=True)
+
+# def index(request):
+#     posts = Movies.objects.all()
+#
+#     context = {
+#         'posts': posts,
+#         'title': 'Главная страница',
+#         'cat_selected': 0,
+#     }
+#     return render(request, 'movies/index.html', context=context)
 
 def about(request):
     return render(request, 'movies/about.html', {'menu': menu, 'title': "ABOUT"})
 
 
-def addpage(request):
-    if request.method == 'POST':
-        form = AddPostForm(request.POST, request.FILES)
-        if form.is_valid():
-            # print(form.cleaned_data)
-            form.save()
-            return redirect('home')
-    else:
-        form = AddPostForm()
-    return render(request, 'movies/addpage.html', {'form': form, 'title': 'Добавление статьи'})
+class AddPage(DataMixin, CreateView):
+    form_class = AddPostForm
+    template_name = 'movies/addpage.html'
+    success_url = reverse_lazy('home')
+    login_url = reverse_lazy('home')
+    raise_exception = True
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+       context = super().get_context_data(**kwargs)
+       c_def = self.get_user_context(title="Добавление статьи")
+       return dict(list(context.items()) + list(c_def.items()))
 
 
+# def addpage(request):
+#     if request.method == 'POST':
+#         form = AddPostForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             # print(form.cleaned_data)
+#             form.save()
+#             return redirect('home')
+#     else:
+#         form = AddPostForm()
+#     return render(request, 'movies/addpage.html', {'form': form, 'title': 'Добавление статьи'})
 def contact(request):
     return HttpResponse('Обратная связь')
 
@@ -38,32 +66,55 @@ def contact(request):
 def login(request):
     return HttpResponse("Войти")
 
+# def show_post(request, post_slug):
+#     post = get_object_or_404(Movies, slug=post_slug)
+#
+#     context = {
+#         'post': post,
+#         'title': post.title,
+#         'cat_selected': post.cat_id,
+#     }
+#     return render(request, 'movies/post.html', context=context)
+class ShowPost(DataMixin, DetailView):
+    model = Movies
+    template_name = 'movies/post.html'
+    slug_url_kwarg = 'post_slug'
+    context_object_name = 'post'
 
-def show_post(request, post_slug):
-    post = get_object_or_404(Movies, slug=post_slug)
-
-    context = {
-        'post': post,
-        'title': post.title,
-        'cat_selected': post.cat_id,
-    }
-    return render(request, 'movies/post.html', context=context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title=context['post'])
+        return dict(list(context.items()) + list(c_def.items()))
 
 
-def show_category(request, cat_slug):
-    posts = Movies.objects.filter(cat__slug=cat_slug)
+class MoviesCategory(DataMixin, ListView):
+    model = Movies
+    template_name = 'movies/index.html'
+    context_object_name = 'posts'
+    allow_empty = False
 
-    if len(posts) == 0:
-        raise Http404()
+    def get_queryset(self):
+        return Movies.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
 
-    context = {
-        'posts': posts,
-        'title': 'Главная страница',
-        'cat_selected': cat_slug,
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Категория - ' + str(context['posts'][0].cat),
+                                      cat_selected=context['posts'][0].cat_id)
+        return dict(list(context.items()) + list(c_def.items()))
 
-    }
-    return render(request, 'movies/index.html', context=context)
-
+# def show_category(request, cat_slug):
+#     posts = Movies.objects.filter(cat__slug=cat_slug)
+#
+#     if len(posts) == 0:
+#         raise Http404()
+#
+#     context = {
+#         'posts': posts,
+#         'title': 'Главная страница',
+#         'cat_selected': cat_slug,
+#
+#     }
+#     return render(request, 'movies/index.html', context=context)
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('Страница не найдена')
